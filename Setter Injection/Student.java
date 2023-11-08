ead255bd9a76491510a9820411bb86b335c60f29
SurturFTW <pushkarsane07@gmail.com>
2023-11-08 23:44:09 +0530

    /*Practical 5C*/

--- Setter Injection/Student.java
+++ Setter Injection/Student.java
@@ -1,5 +1,20 @@
 package practical5c;
 
 public class Student {
+	private Address address;
 
+// Setter method for address
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+// Getter method for address
+	public Address getAddress() {
+		return address;
+	}
+
+	public void printStudentDetails() {
+		System.out.println("Student Details:");
+		System.out.println("Student's " + address.toString());
+	}
 }
